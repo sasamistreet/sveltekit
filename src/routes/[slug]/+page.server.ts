50bb830ec1167ad77d5f603aa4906be1a0c95abb
sasamistreet <39979411+sasamistreet@users.noreply.github.com>
2023-01-25 14:07:59 +0900

    add blog

---
+++ src/routes/[slug]/+page.server.ts
@@ -0,0 +1,10 @@
+import { getDetail } from "../../libs/microcms";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ params }) => {
+    return await getDetail(
+        params.slug
+    );
+};
+
+export const prerender = true;
